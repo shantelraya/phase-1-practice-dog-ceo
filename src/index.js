0d014184a59def50b0e8e0d52ad81ac0c66811ed
shantelraya <parischax@gmail.com>
2022-09-08 13:13:21 +0300

    done assignments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+let breeds = []
+
+document.addEventListener("DOMContentLoaded", function () {
+    dogImages();
+    dogBreeds();
+    filterBreeds();
+})
+
+function dogImages(){
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => {
+    const images  = document.getElementById("dog-image-container");
